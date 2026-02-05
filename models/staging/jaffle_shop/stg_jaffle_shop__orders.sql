4d1113fb85a10768179b10827af99a32cef1e22d
Rajani B <rajani.bharatha@gmail.com>
2026-02-05 03:13:10 +0000

    removed marketing and finanace folders

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,22 +1,6 @@
-with 
-
-source as (
-
-    select * from {{ source('jaffle_shop', 'orders') }}
-
-),
-
-renamed as (
-
-    select
-        id,
-        user_id,
-        order_date,
-        status,
-        _etl_loaded_at
-
-    from source
-
-)
-
-select * from renamed
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from {{ source('jaffle_shop', 'orders') }}
